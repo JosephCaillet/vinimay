f5b65089cc21300f29e329c440a602488d05ed50
Joseph <josephcaillet@gmail.com>
2017-04-09 19:50:21 +0200

    upd db

--- doc/db/createDB.sql
+++ doc/db/createDB.sql
@@ -1,42 +1,34 @@
-#------------------------------------------------------------
-#        Script SQLite  
-#------------------------------------------------------------
+--------------------------------------------------------------
+--        Script SQLite  
+--------------------------------------------------------------
 
 
-#------------------------------------------------------------
-# Table: Post
-#------------------------------------------------------------
+--------------------------------------------------------------
+-- Table: Post
+--------------------------------------------------------------
 CREATE TABLE Post(
 	creationTs          INTEGER NOT NULL ,
 	lastModificationTs  INTEGER NOT NULL ,
 	content             TEXT NOT NULL ,
 	privacy             TEXT NOT NULL ,
-	username            TEXT ,
-	url                 TEXT ,
-	PRIMARY KEY (creationTs) ,
-	
-	FOREIGN KEY (username) REFERENCES Friend(username),
-	FOREIGN KEY (url) REFERENCES Friend(url)
+	PRIMARY KEY (creationTs)
 );
 
 
-#------------------------------------------------------------
-# Table: Friend
-#------------------------------------------------------------
-CREATE TABLE Friend(
-	username        TEXT NOT NULL ,
-	url             TEXT NOT NULL ,
-	description     TEXT ,
-	idToken         TEXT ,
-	signatureToken  TEXT ,
-	status          TEXT ,
+--------------------------------------------------------------
+-- Table: Profile
+--------------------------------------------------------------
+CREATE TABLE Profile(
+	username     TEXT NOT NULL ,
+	url          TEXT NOT NULL ,
+	description  TEXT ,
 	PRIMARY KEY (username,url)
 );
 
 
-#------------------------------------------------------------
-# Table: Comments
-#------------------------------------------------------------
+--------------------------------------------------------------
+-- Table: Comments
+--------------------------------------------------------------
 CREATE TABLE Comments(
 	creationTs          INTEGER NOT NULL ,
 	lastModificationTs  INTEGER NOT NULL ,
@@ -46,36 +38,55 @@ CREATE TABLE Comments(
 	url                 TEXT ,
 	PRIMARY KEY (creationTs) ,
 	
-	FOREIGN KEY (creationTs_Post) REFERENCES Post(creationTs),
-	FOREIGN KEY (username) REFERENCES Friend(username),
-	FOREIGN KEY (url) REFERENCES Friend(url)
+	FOREIGN KEY (creationTs_Post) REFERENCES Post(creationTs) ON DELETE CASCADE,
+	FOREIGN KEY (username) REFERENCES Profile(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (url) REFERENCES Profile(url) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
-#------------------------------------------------------------
-# Table: User
-#------------------------------------------------------------
+--------------------------------------------------------------
+-- Table: User
+--------------------------------------------------------------
 CREATE TABLE User(
-	username     TEXT NOT NULL ,
-	description  TEXT NOT NULL ,
-	password     TEXT NOT NULL ,
-	salt         TEXT NOT NULL ,
-	PRIMARY KEY (username)
+	password  TEXT NOT NULL ,
+	salt      TEXT NOT NULL ,
+	username  TEXT NOT NULL ,
+	url       TEXT NOT NULL ,
+	PRIMARY KEY (username,url) ,
+	
+	FOREIGN KEY (username) REFERENCES Profile(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (url) REFERENCES Profile(url) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
-#------------------------------------------------------------
-# Table: Reaction
-#------------------------------------------------------------
+--------------------------------------------------------------
+-- Table: Friend
+--------------------------------------------------------------
+CREATE TABLE Friend(
+	id_token         TEXT NOT NULL ,
+	signature_token  TEXT NOT NULL ,
+	status           TEXT NOT NULL ,
+	username         TEXT NOT NULL ,
+	url              TEXT NOT NULL ,
+	PRIMARY KEY (username,url) ,
+	
+	FOREIGN KEY (username) REFERENCES Profile(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (url) REFERENCES Profile(url) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+--------------------------------------------------------------
+-- Table: Reaction
+--------------------------------------------------------------
 CREATE TABLE Reaction(
 	creationTs  INTEGER NOT NULL ,
 	username    TEXT NOT NULL ,
 	url         TEXT NOT NULL ,
 	PRIMARY KEY (creationTs,username,url) ,
 	
-	FOREIGN KEY (creationTs) REFERENCES Post(creationTs),
-	FOREIGN KEY (username) REFERENCES Friend(username),
-	FOREIGN KEY (url) REFERENCES Friend(url)
+	FOREIGN KEY (creationTs) REFERENCES Post(creationTs) ON DELETE CASCADE,
+	FOREIGN KEY (username) REFERENCES Profile(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (url) REFERENCES Profile(url) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
