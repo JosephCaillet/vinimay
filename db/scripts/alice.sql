c8ba249dd29a62bd2893c6ad59fab8c92d400935
Joseph <josephcaillet@gmail.com>
2017-04-26 18:10:42 +0200

    populate alice db

--- db/scripts/alice.sql
+++ db/scripts/alice.sql
@@ -9,3 +9,29 @@ VALUES('bob', 'localhost:3001', 'Hello, I''m Bob');
 
 INSERT INTO friend(username, url, id_token, signature_token, status)
 VALUES('bob', 'localhost:3001', 'idtoken', 'sigtoken', 'accepted');
+
+INSERT INTO profile(username, url, description)
+VALUES('caroline', 'localhost:3002', 'Hello, I''m Caroline');
+
+INSERT INTO friend(username, url, id_token, signature_token, status)
+VALUES('caroline', 'localhost:3002', 'idtoken', 'sigtoken', 'accepted');
+
+INSERT INTO profile(username, url, description)
+VALUES('david', 'localhost:3003', 'Hello, I''m David');
+
+INSERT INTO friend(username, url, id_token, signature_token, status)
+VALUES('david', 'localhost:3003', 'idtoken', 'sigtoken', 'pending');
+
+INSERT INTO profile(username, url, description)
+VALUES('eric', 'localhost:3004', 'Hello, I''m Eric');
+
+INSERT INTO friend(username, url, id_token, signature_token, status)
+VALUES('eric', 'localhost:3004', 'idtoken', 'sigtoken', 'declined');
+
+INSERT INTO profile(username, url, description)
+VALUES('francis', 'localhost:3005', 'Hello, I''m Francis');
+
+INSERT INTO friend(username, url, id_token, signature_token, status)
+VALUES('francis', 'localhost:3005', 'idtoken', 'sigtoken', 'incoming');
+
+--export enum Status {pending, declined, incoming, accepted, following}
