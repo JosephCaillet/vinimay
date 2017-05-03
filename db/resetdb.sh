ac0eb4fa4e4a43d7880cb32ef11c672e6247c943
Brendan Abolivier <contact@brendanabolivier.com>
2017-05-03 14:41:56 +0200

    Removed deprecated script

--- db/resetdb.sh
+++
@@ -1,8 +0,0 @@
-rm *.db > /dev/null 2>&1
-
-users=$(find scripts -name "*.sql" -printf "%f\n" | cut -d. -f1)
-
-for i in $users; do
-	cat ../doc/db/createDB.sql | sqlite3 $i.db
-	cat scripts/$i.sql | sqlite3 $i.db
-done
