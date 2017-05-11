d5959f1b185cf8bcd6fd0b5edfd639b5dcf4b38f
Brendan Abolivier <contact@brendanabolivier.com>
2017-05-11 08:53:55 +0200

    Removed build and servers start/stop in tests file

--- run_tests.sh
+++ run_tests.sh
@@ -2,18 +2,7 @@
 
 # Build
 
-echo 'Building'
-
-rm -rf build
-tsc -p server
-
-if [[ $? -eq 1 ]]; then
-	echo 'Build failed'
-	exit 1
-fi
-
 ./resetdb.sh
-./starTestInstances.sh start
 
 # Let the servers start
 sleep 2
@@ -27,9 +16,6 @@ newman run tests/me.json
 codes="$codes $?"
 newman run tests/posts.json
 codes="$codes $?"
-
-./starTestInstances.sh stop
-./starTestInstances.sh clean
 
 if [[ $codes == *"1"* ]]; then
 	exit 1
