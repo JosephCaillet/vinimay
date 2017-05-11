ae3b95e8802026a0512bee7e54aca65f9b5cf128
Joseph <josephcaillet@gmail.com>
2017-05-11 15:26:42 +0200

    external config, with live loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { AddProfileModal } from './../components/add-profile-modal/add-profile-modal';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, APP_INITIALIZER } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { ConfigurationService } from "ionic-configuration-service";
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -16,7 +17,7 @@ import { BASE_PATH } from '../providers/apiClient/variables';
 import { PostModal } from "../components/post-modal/post-modal";
 import { Autoresize } from "../components/autoresize/autoresize";
 import DateFormaterService from "../providers/date-formater";
-let Config = require("../config");
+//let Config = require("../config");
 
 @NgModule({
 	declarations: [
@@ -54,7 +55,16 @@ let Config = require("../config");
 	providers: [
 		StatusBar,
 		SplashScreen,
-		[V1Service, { provide: BASE_PATH, useValue: Config.apiEndpoint }],
+		ConfigurationService,
+		{
+			provide: APP_INITIALIZER,
+      useFactory: loadConfiguration,
+      deps: [ConfigurationService],
+			multi: true
+		},
+		{ provide: V1Service, useFactory: createAPIEndpointLoader, deps: [Http, ConfigurationService] },
+		//[V1Service, { provide: BASE_PATH, useFactory: (createAPIEndpointLoader), deps: [Http] }],
+		//[V1Service, { provide: BASE_PATH, useValue: Config.apiEndpoint }],
 		// { provide: LOCALE_ID, useValue: 'en-EN' },
 		{ provide: ErrorHandler, useClass: IonicErrorHandler },
 		DateFormaterService
@@ -64,4 +74,12 @@ export class AppModule { }
 
 export function createTranslateLoader(http: Http) {
 	return new TranslateHttpLoader(http, './assets/i18n/', '.json')
+}
+
+export function loadConfiguration(conf: ConfigurationService) {
+	return () => conf.load("assets/config.json");
+}
+
+export function createAPIEndpointLoader(http: Http, conf: ConfigurationService) {
+	return new V1Service(http, conf.getValue<string>('apiEndpoint'), null)
 }
